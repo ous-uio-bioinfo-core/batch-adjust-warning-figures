976458bf0c3b643ca60454df634b493bb4b9a88a
vegardny <vegardsemail@gmail.com>
2014-06-18 17:22:16 +0200

    figure1 fix

--- overviewplot.r
+++ overviewplot.r
@@ -4,7 +4,7 @@
 
 
 library(sva)
-
+library(lsmeans)
  
 adhocboxplot = function(y, grouplabels, batchlabels, addlegend=FALSE, ylim=NULL, xlab="x", figureletter=NA)
 {
@@ -39,9 +39,7 @@ adhocboxplot = function(y, grouplabels, batchlabels, addlegend=FALSE, ylim=NULL,
   boxseparation = 0.25
   for(g in groupnames)
   { 
-    #boxplot(y[grouplabels==g], notch=TRUE, border=colpalette[g], 
-    #        add=TRUE, at=xoffset, col=rgb(1,0.5,0.3, alpha=0.0),
-    #        outline=FALSE, boxwex=0.5)
+
     m = mean(y[grouplabels==g])
     ci = t.test(y[grouplabels==g])[["conf.int"]]
     xleft = xoffset
@@ -67,6 +65,61 @@ adhocboxplot = function(y, grouplabels, batchlabels, addlegend=FALSE, ylim=NULL,
   	text(x=0, y=ylim[2], labels=figureletter, cex=legendcex)
   }
 }
+
+
+
+adhocboxplot2 = function(y, grouplabels, batchlabels, addlegend=FALSE, ylim=NULL, xlab="x", figureletter=NA)
+{
+	#x = batchlabels + grouplabels/8
+	colpalette = c("blue", "red", "darkgreen", "magenta")
+	pchselection = c(1, 2, 3)
+	x = batchlabels
+	lwd=2
+	
+	
+	xlim = c(0, length(unique(batchlabels))+2)
+	plot(1, 1, col=colpalette[grouplabels], pch=pchselection[batchlabels], 
+			 xlim=xlim, lwd=lwd, cex=2, ylim=ylim, xlab=xlab, ylab="", axes=FALSE, cex.lab =2, type="n")
+	
+	expression =  matrix_conditionbatch[1,]
+	group= factor(grouplabels)
+	batch= factor(batchlabels)
+	fit = lm(expression ~ group + batch )
+	anovaest = summary(lsmeans(fit,  ~group))
+	
+	
+	groupnames = unique(grouplabels)
+	groupnames = groupnames[order(groupnames)]
+	xoffset=2
+	boxwidth=0.35
+	boxseparation = 0.25
+	for(g in groupnames)
+	{ 
+		m =anovaest[g , "lsmean"]		
+		xleft = xoffset
+		ybottom = anovaest[g , "lower.CL"]		
+		xright = xleft + boxwidth
+		ytop = anovaest[g , "upper.CL"]	
+		rect(xleft, ybottom, xright, ytop, border=colpalette[g], lwd=lwd)
+		lines(c(xoffset,xright), c(m,m), col=colpalette[g], lwd=lwd)
+		xoffset = xoffset+boxseparation
+	}
+	legendcex=2
+	if(addlegend)
+	{
+		
+		legend("topright", legend=paste("Group ", groupnames, sep=""), 
+					 text.col=colpalette[groupnames], bty="n", cex=legendcex)
+		legend("bottomright", legend=paste("Batch ", unique(batchlabels), sep=""),
+					 pch=pchselection[unique(batchlabels)], bty="n", cex=legendcex)
+	}
+	if(!is.na(figureletter))
+	{
+		figureletter=paste("(", figureletter, ")", sep="")
+		text(x=0, y=ylim[2], labels=figureletter, cex=legendcex)
+	}
+}
+
 
 source("commonscripts/helperfunctions.r")
 
@@ -97,12 +150,10 @@ matrix_combat = ComBat(dat=matrix_conditionbatch, batch=sampleannotation$batch,
 
 
 
-index=1
-#figfile = paste( getwd(), "/", "figure1_", rseed, ".pdf", sep="")
-figfile = paste( getwd(), "/", "figure1",  ".pdf", sep="")
-pdf(file =figfile, width=16, height=8)
-#png(file = paste("figure/", "figure1_", rseed, ".png", sep=""), width=1600, height=800)
-par(mfrow=c(1, 4))
+
+figfile = paste( getwd(), "/", "boxplots",  ".pdf", sep="")
+pdf(file =figfile, width=32, height=16)
+par(mfrow=c(1, 5))
 
 alldata = c(matrix_condition[index,],
             matrix_conditionbatch[index,],
@@ -112,36 +163,11 @@ ylim = c(min(alldata), max(alldata))
 adhocboxplot(matrix_condition[index,], sampleannotation$treatment, sampleannotation$batch, addlegend=FALSE, ylim=ylim, xlab="Without batch effects", figureletter="a")
 adhocboxplot(matrix_conditionbatch[index,], sampleannotation$treatment, sampleannotation$batch, addlegend=FALSE, ylim=ylim, xlab="With batch effects added", figureletter="b")
 adhocboxplot(matrix_meancenter[index,], sampleannotation$treatment, sampleannotation$batch, addlegend=FALSE, ylim=ylim, xlab="Zero-centered per batch", figureletter="c")
-adhocboxplot(matrix_combat[index,], sampleannotation$treatment, sampleannotation$batch, addlegend=TRUE, ylim=ylim, xlab="ANOVA centered values (for tiden combat)", figureletter="d")
+adhocboxplot(matrix_combat[index,], sampleannotation$treatment, sampleannotation$batch, addlegend=FALSE, ylim=ylim, xlab="ANOVA centered values (for tiden combat)", figureletter="d")
+adhocboxplot2(matrix_conditionbatch[index,], sampleannotation$treatment, sampleannotation$batch, addlegend=TRUE, ylim=ylim, xlab="ANOVA estimates", figureletter="e")
 dev.off()
 
 print( paste("Figure created; ",figfile ))
 
-
-
-# for(i in 1:100)
-# {
-#   
-# index=i
-# png(file = paste("figure/", "boxplotgene_", index,".png", sep=""), width=1600, height=800)
-# par(mfrow=c(1, 4))
-# 
-# alldata = c(matrix_condition[index,],
-#             matrix_conditionbatch[index,],
-#             matrix_meancenter[index,],
-#             matrix_combat[index,])
-# ylim = c(min(alldata), max(alldata))
-# #adhocboxplot(matrix_random[index,], sampleannotation$treatment, sampleannotation$batch, addlegend=TRUE)
-# adhocboxplot(matrix_condition[index,], sampleannotation$treatment, sampleannotation$batch, addlegend=FALSE, ylim=ylim, xlab="Without batch effects")
-# adhocboxplot(matrix_conditionbatch[index,], sampleannotation$treatment, sampleannotation$batch, addlegend=FALSE, ylim=ylim, xlab="With batch effects added")
-# adhocboxplot(matrix_meancenter[index,], sampleannotation$treatment, sampleannotation$batch, addlegend=FALSE, ylim=ylim, xlab="Zero-centered per batch")
-# adhocboxplot(matrix_combat[index,], sampleannotation$treatment, sampleannotation$batch, addlegend=TRUE, ylim=ylim, xlab="ANOVA centered values (for tiden combat)")
-# 
-# #aov(matrix_condition[index,] ~ sampleannotation$treatment+sampleannotation$batch )
-# #aov(matrix_condition[index,] ~ as.character(sampleannotation$treatment))
-# dev.off()
-# 
-# 
-# }
 
 
